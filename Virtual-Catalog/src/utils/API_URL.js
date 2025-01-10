2937c50bb07a5625900b73233d4e16ba04a63911
Christofer Daniel Chaves Pauth <42120690+Christofer2002@users.noreply.github.com>
2025-01-10 14:43:26 -0600

    Update API_URL.js

--- Virtual-Catalog/src/utils/API_URL.js
+++ Virtual-Catalog/src/utils/API_URL.js
@@ -1,9 +1,11 @@
-const API_URL = 'https://localhost:7278/api/';
+const API_URL_VPS = 'https://www.devbychris.com/virtual-catalog/api/';
 
-const AUTH_URL = API_URL + 'auth/';
-const CATEGORY_URL = API_URL + 'category/';
-const PRODUCT_URL = API_URL + 'product/';
-const USER_URL = API_URL + 'user/';
-const ROLE_URL = API_URL + 'role/';
+const API_URL_DEV = 'http://localhost:7278/api/';
+
+const AUTH_URL = API_URL_VPS + 'auth/';
+const CATEGORY_URL = API_URL_VPS + 'category/';
+const PRODUCT_URL = API_URL_VPS + 'product/';
+const USER_URL = API_URL_VPS + 'user/';
+const ROLE_URL = API_URL_VPS + 'role/';
 
 export { AUTH_URL, CATEGORY_URL, PRODUCT_URL, USER_URL, ROLE_URL };
